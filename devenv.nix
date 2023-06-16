6297ed9388270eff2666412fe39fabafcb185105
a <a@as-Virtual-Machine.local>
2023-06-16 13:13:23 -0700

    Update .gitignore and devenv.nix for pre-commit hooks

--- devenv.nix
+++ devenv.nix
@@ -27,6 +27,18 @@
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks = {
+    nixpkgs-fmt.enable = true;
+    ormolu.enable = true;
+    prettier.enable = true;
+    # https://github.com/cachix/pre-commit-hooks.nix/issues/31#issuecomment-744657870
+    trailing-whitespace = {
+      enable = true;
+      # https://github.com/pre-commit/pre-commit-hooks/blob/4b863f127224b6d92a88ada20d28a4878bf4535d/.pre-commit-hooks.yaml#L201-L207
+      entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
+      types = [ "text" ];
+    };
+  };
 
   # https://devenv.sh/processes/
   # processes.ping.exec = "ping example.com";
