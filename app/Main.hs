d3351dc4e0bb0dbce12cd3d404a23c6ba91bacef
a <a@as-Virtual-Machine.local>
2023-06-16 14:13:02 -0700

    Update Main.hs to watch plist files

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main (main) where
 
+import Control.Monad (forever)
+import GHC.IO.Handle (hGetLine)
+import System.Process (CreateProcess (std_out), StdStream (CreatePipe), createProcess, proc)
 import Prelude
 
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+  putStrLn "Watching plist files..."
+  let fswatchArgs = ["-r", "--include=.*\\.plist$", "--exclude=.*", "/"]
+  (_, Just hout, _, _) <- createProcess (proc "fswatch" fswatchArgs) {std_out = CreatePipe}
+  forever $ do
+    path <- hGetLine hout
+    printPlistFile path
+
+printPlistFile :: FilePath -> IO ()
+printPlistFile path = do
+  putStrLn $ "Plist file changed: " ++ path
+  return ()
