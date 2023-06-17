273ee8fee8837a47dde85cb37947d6a6c5e572b5
a <a@as-Virtual-Machine.local>
2023-06-16 18:36:02 -0700

    Add callPlistBuddy function to Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,15 @@ module Main (main) where
 
 import Control.Monad (forever)
 import GHC.IO.Handle (hGetLine)
-import System.Process (CreateProcess (std_out), StdStream (CreatePipe), createProcess, proc)
+import System.Exit (ExitCode (..))
+import System.Process (CreateProcess (std_out), StdStream (CreatePipe), createProcess, proc, readProcessWithExitCode)
 import Prelude
+
+callPlistBuddy :: String -> FilePath -> IO (ExitCode, String)
+callPlistBuddy command path = do
+  let plistBuddyArgs = ["-c", command, path]
+  (exitCode, output, _) <- readProcessWithExitCode "/usr/libexec/PlistBuddy" plistBuddyArgs ""
+  return (exitCode, output)
 
 main :: IO ()
 main = do
