12be599c988b68b89d8b20e854a817ce96762515
a <a@as-Virtual-Machine.local>
2023-06-17 18:44:55 -0700

    Include hspec dependency and update test suite in test/Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,9 @@
+import Test.Hspec (describe, hspec, it, shouldBe)
 import Prelude
 
+-- https://hspec.github.io/#:~:text=main%20%3A%3A%20io%20()%20main%20%3D%20hspec%20%24%20do%20describe%20%22prelude.head%22%20%24%20do%20it%20%22returns%20the%20first%20element%20of%20a%20list%22%20%24%20do%20head%20%5B23%20..%5D%20%60shouldbe%60%20(23%20%3A%3A%20int)
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Prelude.head" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
