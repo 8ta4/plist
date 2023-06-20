ff9270841a9b8e99ad4b7aef2d5193510b5081ef
a <a@as-Virtual-Machine.local>
2023-06-20 16:38:20 -0700

    Implement basic functionality of flattenJSON

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,12 @@
 module Lib (flattenJSON) where
 
-import Data.Aeson (Value)
+import Data.Aeson (Object, Value (..))
+import Data.Text (Text)
 import Prelude
 
 flattenJSON :: Value -> Value
-flattenJSON = undefined
+flattenJSON (Object obj) = Object $ flattenObject obj ""
+flattenJSON val = val
+
+flattenObject :: Object -> Text -> Object
+flattenObject obj prefix = obj
