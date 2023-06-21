02c7da5bf18656d24f23aac851f2509ebac724b5
a <a@as-Virtual-Machine.local>
2023-06-20 17:44:07 -0700

    Implement flattenObject function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib (flattenObject) where
 
-import Data.Aeson (Object)
+import Data.Aeson (Object, Value (Object))
+import Data.Aeson.KeyMap (fromHashMapText, toHashMapText)
+import Data.HashMap.Strict (fromList, toList)
+import Data.Text qualified as T
+import Prelude
 
 flattenObject :: Object -> Object
-flattenObject obj = obj
+flattenObject = flattenObject' ""
+
+flattenObject' :: T.Text -> Object -> Object
+flattenObject' prefix obj = fromHashMapText $ fromList $ concatMap process $ toList $ toHashMapText obj
+  where
+    process (key, value) = case value of
+      Object nested -> toList $ toHashMapText $ flattenObject' (prefix <> ":" <> key) nested
+      _ -> [(prefix <> ":" <> key, value)]
