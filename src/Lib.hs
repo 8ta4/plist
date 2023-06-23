81605d1520c2c4bf62bd2b04e308935498d0ab0d
a <a@as-Virtual-Machine.local>
2023-06-23 01:45:34 -0700

    Add support for flattening arrays in flattenObject

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,10 @@
 module Lib (flattenObject) where
 
-import Data.Aeson (Object, Value (Object))
+import Data.Aeson (Object, Value (Array, Object))
 import Data.Aeson.KeyMap (fromHashMapText, toHashMapText)
-import Data.HashMap.Strict (fromList, toList)
+import Data.Foldable (toList)
+import Data.HashMap.Strict (fromList)
+import Data.HashMap.Strict qualified as HashMap
 import Data.Text qualified as T
 import Prelude
 
@@ -10,8 +12,10 @@ flattenObject :: Object -> Object
 flattenObject = flattenObject' ""
 
 flattenObject' :: T.Text -> Object -> Object
-flattenObject' prefix obj = fromHashMapText $ fromList $ concatMap process $ toList $ toHashMapText obj
+flattenObject' prefix obj = fromHashMapText $ fromList $ concatMap process $ HashMap.toList $ toHashMapText obj
   where
+    process :: (T.Text, Value) -> [(T.Text, Value)]
     process (key, value) = case value of
-      Object nested -> toList $ toHashMapText $ flattenObject' (prefix <> ":" <> key) nested
+      Object nested -> HashMap.toList $ toHashMapText $ flattenObject' (prefix <> ":" <> key) nested
+      Array arr -> concatMap (\(i, v) -> process (key <> ":" <> T.pack (show i), v)) (zip [0 :: Int ..] (toList arr))
       _ -> [(prefix <> ":" <> key, value)]
